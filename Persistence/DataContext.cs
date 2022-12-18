8e28732492084cbb010cf3ff2d294ca55db49d7c
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2022-12-18 21:52:35 +0100

    Database context added

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Domain;
+
+namespace Persistence;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Activity> Activities { get; set; }
+}
