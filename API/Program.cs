f5d31fcf01b68538f9a202b1c919e110f38c03d8
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2022-12-19 18:06:50 +0100

    Data seed added

--- API/Program.cs
+++ API/Program.cs
@@ -29,4 +29,18 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedData(context);
+}
+catch (Exception e)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(e, "An error occured during migration.");
+}
+
 app.Run();
