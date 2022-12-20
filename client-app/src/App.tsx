908416b44ce7554e81190ec0c7bd8f2137f8b45d
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2022-12-20 17:32:08 +0100

    Axios added to client-app, fetching data from api.

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,28 +1,31 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {ducks} from "./demo";
-import DuckItem from "./DuckItem";
+import axios from "axios";
 
 function App() {
+
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities')
+        .then(response => {
+          console.log(response);
+          setActivities(response.data);
+        })
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {ducks.map(duck => (
-            <DuckItem duck={duck}  key={duck.name}/>
-        ))}
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          {activities.map((activity: any) => (
+              <li key={activity.id}>
+                {activity.title}
+              </li>
+          ))}
+        </ul>
       </header>
     </div>
   );
