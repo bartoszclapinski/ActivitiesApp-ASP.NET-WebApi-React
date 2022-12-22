cb3a808563ae01569d66ec7aae20c951eb59e200
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2022-12-22 23:41:45 +0100

    Delete endpoint added.

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -34,4 +34,10 @@ public class ActivitiesController : BaseApiController
         activity.Id = id;
         return Ok(await Mediator.Send(new Edit.Command { Activity = activity }));
     }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteActivity(Guid id)
+    {
+        return Ok(await Mediator.Send(new Delete.Command { Id = id }));
+    }
 }
---
+++ Application/Activities/Delete.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Persistence;
+
+namespace Application.Activities;
+
+public class Delete
+{
+    public class Command : IRequest
+    {
+        public Guid Id { get; set; }
+    }
+
+    public class Handler : IRequestHandler<Command>
+    {
+        private readonly DataContext _context;
+
+        public Handler(DataContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+        
+        public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+        {
+            var activity = await _context.Activities.FindAsync(request.Id);
+
+            _context.Remove(activity);
+
+            return Unit.Value;
+        }
+    }
+}
