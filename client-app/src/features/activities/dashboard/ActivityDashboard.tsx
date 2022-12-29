5795917ea2dd2f118c7db13885af4de808405a07
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2022-12-29 13:27:58 +0100

    Activity details card component added.

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -2,6 +2,7 @@ import {Grid, List} from "semantic-ui-react";
 import React from "react";
 import {Activity} from "../../../app/models/activity";
 import ActivityList from "./ActivityList";
+import ActivityDetails from "../details/ActivityDetails";
 
 interface Props {
     activities: Activity[];
@@ -12,6 +13,9 @@ export default function ActivityDashboard({activities}: Props) {
         <Grid>
             <Grid.Column width='10'>
                 <ActivityList activities={activities} />
+            </Grid.Column>
+            <Grid.Column width='6'>
+                {activities[0] && <ActivityDetails activity={activities[0]} />}
             </Grid.Column>
         </Grid>
     )
