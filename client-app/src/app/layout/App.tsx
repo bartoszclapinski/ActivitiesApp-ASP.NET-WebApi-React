824c2d97f2bac635d4fe81dc8213ef27f00c5aa7
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2022-12-29 12:12:55 +0100

    Activity dashboard component added.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,8 +1,9 @@
 import React, {Fragment, useEffect, useState} from 'react';
 import axios from "axios";
-import {Container, Header, List} from "semantic-ui-react";
+import {Container} from "semantic-ui-react";
 import {Activity} from "../models/activity";
 import NavBar from "./NavBar";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 
 function App() {
 
@@ -19,13 +20,7 @@ function App() {
     <Fragment>
       <NavBar />
         <Container style={{marginTop: '7em'}}>
-            <List>
-                {activities.map((activity) => (
-                    <List.Item key={activity.id}>
-                        {activity.title}
-                    </List.Item>
-                ))}
-            </List>
+            <ActivityDashboard activities={activities} />
         </Container>
     </Fragment>
   );
