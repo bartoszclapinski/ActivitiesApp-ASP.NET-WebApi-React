c8e051b19978be4fbc5bc0b726a312ebf7dcce4b
Bartosz Cłapiński <bartosz.clapinski@gmail.com>
2022-12-31 21:44:41 +0100

    MobX setup

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -6,8 +6,11 @@ import ActivityDashboard from "../../features/activities/dashboard/ActivityDashb
 import {v4 as uuid} from 'uuid';
 import agent from "../api/agent";
 import LoadingComponent from "./LoadingComponent";
+import {useStore} from "../stores/store";
 
 function App() {
+
+  const {activityStore} = useStore();
 
   const [activities, setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined> (undefined);
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -4,12 +4,15 @@ import 'semantic-ui-css/semantic.min.css';
 import './app/layout/styles.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
+import {store, StoreContext} from "./app/stores/store";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <App />
+    <StoreContext.Provider value={store}>
+        <App />
+    </StoreContext.Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
