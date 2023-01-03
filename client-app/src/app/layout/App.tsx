50ac162c0d9e3386850b3394d85f390b0af741bc
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-01-03 21:55:48 +0100

    Adding routes.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,28 +1,16 @@
-import React, {Fragment, useEffect} from 'react';
+import React, {Fragment} from 'react';
 import {Container} from "semantic-ui-react";
 import NavBar from "./NavBar";
-import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
-import LoadingComponent from "./LoadingComponent";
-import {useStore} from "../stores/store";
 import {observer} from "mobx-react-lite";
+import {Outlet} from "react-router-dom";
 
 function App() {
-
-  const {activityStore} = useStore();
-
-  useEffect(() => {
-      activityStore.loadActivities();
-  }, [activityStore]);
-
-
-  if(activityStore.loadingInitial)
-      return <LoadingComponent content={"Loading activity data..."} />
 
   return (
     <Fragment>
       <NavBar />
         <Container style={{marginTop: '7em'}}>
-            <ActivityDashboard />
+            <Outlet />
         </Container>
     </Fragment>
   );
