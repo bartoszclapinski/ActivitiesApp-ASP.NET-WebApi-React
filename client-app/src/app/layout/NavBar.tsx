4232f564db96b9b92af494212577ed6855867b52
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-01-03 22:01:05 +0100

    Added nav links.

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
 import {Container, Menu, Button} from "semantic-ui-react";
-import {useStore} from "../stores/store";
-import {observer} from "mobx-react-lite";
+import {NavLink} from "react-router-dom";
 
-export default observer (function NavBar() {
-
-    const {activityStore} = useStore();
+export default function NavBar() {
 
     return (
         <Menu inverted fixed='top'>
             <Container>
-                <Menu.Item header>
+                <Menu.Item as={NavLink} to={'/'} header>
                     <img src="/assets/logo.png" alt="logo" style={{marginRight: '10px'}}/>
                     APPtivities
                 </Menu.Item>
-                <Menu.Item name="Activities"/>
+                <Menu.Item as={NavLink} to={'/activities'} name="Activities"/>
                 <Menu.Item>
-                    <Button onClick={() => activityStore.openForm('')} positive content="Create Activity"/>
+                    <Button as={NavLink} to={'/createActivity'} positive content="Create Activity"/>
                 </Menu.Item>
 
             </Container>
         </Menu>
     )
-});
+};
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,15 +1,24 @@
 import {Grid} from "semantic-ui-react";
-import React from "react";
+import React, {useEffect} from "react";
 import ActivityList from "./ActivityList";
 import ActivityDetails from "../details/ActivityDetails";
 import ActivityForm from "../form/ActivityForm";
 import {useStore} from "../../../app/stores/store";
 import {observer} from "mobx-react-lite";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
 
 export default observer (function ActivityDashboard() {
 
     const {activityStore} = useStore();
     const {selectedActivity, editMode} = activityStore;
+
+    useEffect(() => {
+        activityStore.loadActivities();
+    }, [activityStore]);
+
+
+    if(activityStore.loadingInitial)
+        return <LoadingComponent content={"Loading activity data..."} />
 
     return (
         <Grid>
