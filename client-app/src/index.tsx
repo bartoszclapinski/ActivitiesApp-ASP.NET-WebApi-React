926e6bdb32741a02414e60c2dd2e919eaa29ee39
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-01-03 17:41:28 +0100

    Installing react router.

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -2,16 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'semantic-ui-css/semantic.min.css';
 import './app/layout/styles.css';
-import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 import {store, StoreContext} from "./app/stores/store";
+import {RouterProvider} from "react-router-dom";
+import {router} from "./app/router/Routes";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
     <StoreContext.Provider value={store}>
-        <App />
+        <RouterProvider router={router} />
     </StoreContext.Provider>
 );
 
