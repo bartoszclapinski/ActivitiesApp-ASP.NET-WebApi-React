a6f9e9be70ba2038476962fa36e8a7222a21ee2f
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-01-04 16:41:24 +0100

    Clean up dashboard

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,8 +1,6 @@
 import {Grid} from "semantic-ui-react";
 import React, {useEffect} from "react";
 import ActivityList from "./ActivityList";
-import ActivityDetails from "../details/ActivityDetails";
-import ActivityForm from "../form/ActivityForm";
 import {useStore} from "../../../app/stores/store";
 import {observer} from "mobx-react-lite";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
@@ -10,7 +8,6 @@ import LoadingComponent from "../../../app/layout/LoadingComponent";
 export default observer (function ActivityDashboard() {
 
     const {activityStore} = useStore();
-    const {selectedActivity, editMode} = activityStore;
 
     useEffect(() => {
         activityStore.loadActivities();
@@ -26,11 +23,7 @@ export default observer (function ActivityDashboard() {
                 <ActivityList />
             </Grid.Column>
             <Grid.Column width='6'>
-                {selectedActivity && !editMode &&
-                    <ActivityDetails />}
-                {editMode &&
-                    <ActivityForm />
-                }
+                <h2>Activity filters</h2>
             </Grid.Column>
         </Grid>
     )
