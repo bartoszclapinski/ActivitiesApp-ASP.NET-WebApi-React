bad88aa8187b758cd7f565d3ab5dfdb2a42a0874
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-01-05 14:24:35 +0100

    Activity details styling - Header

---
+++ client-app/src/features/activities/details/ActivityDetailedHeader.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import {observer} from "mobx-react-lite";
+import {Activity} from "../../../app/models/activity";
+import {Button, Header, Image, Item, Segment, SegmentGroup} from "semantic-ui-react";
+
+const activityImageStyle = {
+    filter: 'brightness(30%)'
+};
+
+const activityImageTextStyle = {
+    position: 'absolute',
+    bottom: '5%',
+    left: '5%',
+    width: '100%',
+    height: 'auto',
+    color: 'white'
+};
+
+interface Props {
+    activity: Activity
+}
+
+export default observer (function ActivityDetailedHeader({activity}: Props) {
+    return (
+        <Segment.Group>
+            <Segment basic attached={'top'} style={{padding: '0'}}>
+                <Image src={`/assets/categoryImages/${activity.category}.jpg`} fluid style={activityImageStyle} />
+                <Segment style={activityImageTextStyle} basic >
+                    <Item.Group>
+                        <Item>
+                            <Item.Content>
+                                <Header
+                                    size={'huge'}
+                                    content={activity.title}
+                                    style={{color: 'white'}}
+                                />
+                                <p>{activity.date}</p>
+                                <p>Hosted by <strong>Bob</strong></p>
+                            </Item.Content>
+                        </Item>
+                    </Item.Group>
+                </Segment>
+            </Segment>
+            <Segment clearing attached={'bottom'}>
+                <Button color={'teal'}>Join Activity</Button>
+                <Button>Cancel attendance</Button>
+                <Button color={'orange'} floated={'right'}>Manage Event</Button>
+            </Segment>
+        </Segment.Group>
+    )
+})
