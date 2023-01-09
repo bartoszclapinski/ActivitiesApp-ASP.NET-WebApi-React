2e00fefddacf94817a56e0a558ec86b557a20d2d
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-01-09 19:16:38 +0100

    Home page styling.

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'react-calendar/dist/Calendar.css'
 import 'semantic-ui-css/semantic.min.css';
 import './app/layout/styles.css';
 import reportWebVitals from './reportWebVitals';
