9205ba5ca2447380bf879d21cb11e7065b432ac5
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-01-10 16:08:28 +0100

    React toastify added

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -4,12 +4,14 @@ import NavBar from "./NavBar";
 import {observer} from "mobx-react-lite";
 import {Outlet, useLocation} from "react-router-dom";
 import HomePage from "../../features/home/HomePage";
+import {ToastContainer} from "react-toastify";
 
 function App() {
     const location = useLocation();
 
     return (
         <Fragment>
+            <ToastContainer position={'bottom-right'} hideProgressBar theme={'colored'} />
             { location.pathname === '/' ? <HomePage /> : (
                 <>
                     <NavBar />
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'react-calendar/dist/Calendar.css'
 import 'semantic-ui-css/semantic.min.css';
+import 'react-toastify/dist/ReactToastify.min.css'
 import './app/layout/styles.css';
 import reportWebVitals from './reportWebVitals';
 import {store, StoreContext} from "./app/stores/store";
