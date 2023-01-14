e4401f52572a3123a356a5150739e61314543b02
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-01-14 16:17:06 +0100

    Setting up activity form for validation input with Formik and Yup.

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -8,6 +8,7 @@ import LoadingComponent from "../../../app/layout/LoadingComponent";
 import {v4 as uuid} from "uuid";
 import {Formik, Form, Field} from "formik";
 import * as Yup from 'yup';
+import MyTextInput from "../../../app/common/form/MyTextInput";
 
 
 export default observer (function ActivityForm() {
@@ -28,7 +29,12 @@ export default observer (function ActivityForm() {
     });
 
     const validationSchema = Yup.object({
-        title: Yup.string().required('The activity title is required.');
+        title: Yup.string().required('The activity title is required.'),
+        description: Yup.string().required('The activity description is required.'),
+        category: Yup.string().required('The category is required.'),
+        date: Yup.string().required('The activity date is required.'),
+        venue: Yup.string().required('The activity venue is required.'),
+        city: Yup.string().required('The activity city is required.')
     })
 
     useEffect(() => {
@@ -57,12 +63,12 @@ export default observer (function ActivityForm() {
                     onSubmit={values => console.log(values)}>
                 {({ handleSubmit }) => (
                     <Form className={'ui form'} onSubmit={handleSubmit} autoComplete={'off'}>
-                        <Field placeholder={'Title'} name={'title'} />
-                        <Field placeholder={'Description'} name={'description'} />
-                        <Field placeholder={'Category'} name={'category'} />
-                        <Field type={'date'} placeholder={'Date'} name={'date'} />
-                        <Field placeholder={'City'} name={'city'} />
-                        <Field placeholder={'Venue'} name={'venue'} />
+                        <MyTextInput placeholder={'Title'} name={'title'} />
+                        <MyTextInput placeholder={'Description'} name={'description'} />
+                        <MyTextInput placeholder={'Category'} name={'category'} />
+                        <MyTextInput placeholder={'Date'} name={'date'} />
+                        <MyTextInput placeholder={'City'} name={'city'} />
+                        <MyTextInput placeholder={'Venue'} name={'venue'} />
                         <Button loading={loading} floated={'right'} positive type={'submit'} content={'Submit'} />
                         <Button as={Link} to={'/activities'} floated={'right'} type={'button'} content={'Cancel'} />
                     </Form>
@@ -70,4 +76,4 @@ export default observer (function ActivityForm() {
             </Formik>
         </Segment>
     )
-});
+});MyTextInput
