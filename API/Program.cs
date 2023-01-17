c2512a0a8d62b73c259e9f8a8ee00d22e6b8110b
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-01-17 17:08:49 +0100

    Seed users added

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,7 @@
 using API.Extensions;
 using API.Middleware;
+using Domain;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -35,8 +37,9 @@ var services = scope.ServiceProvider;
 try
 {
     var context = services.GetRequiredService<DataContext>();
+    var userManager = services.GetRequiredService<UserManager<AppUser>>();
     await context.Database.MigrateAsync();
-    await Seed.SeedData(context);
+    await Seed.SeedData(context, userManager);
 }
 catch (Exception e)
 {
