94f4161b090bb434825397dfd3e9b9df409c4137
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-01-17 16:52:39 +0100

    Configuring identity services

---
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Domain;
+using Persistence;
+
+namespace API.Extensions;
+
+public static class IdentityServiceExtensions
+{
+    public static IServiceCollection AddIdentityService(
+        this IServiceCollection services, IConfiguration config)
+    {
+        services.AddIdentityCore<AppUser>(options =>
+        {
+            options.Password.RequireNonAlphanumeric = false;
+        })
+            .AddEntityFrameworkStores<DataContext>();
+        
+        services.AddAuthentication();
+
+        return services;
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityService(builder.Configuration);
 
 
 var app = builder.Build();
