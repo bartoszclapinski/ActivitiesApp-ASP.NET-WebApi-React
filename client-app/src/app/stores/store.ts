0f7cb297cac8ecc3b8da52bbef6b3e5fbbbeb33e
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-01-21 20:10:23 +0100

    User store added

--- client-app/src/app/stores/store.ts
+++ client-app/src/app/stores/store.ts
@@ -1,15 +1,18 @@
 import ActivityStore from "./activityStore";
 import {createContext, useContext} from "react";
 import CommonStore from "./commonStore";
+import UserStore from "./userStore";
 
 interface Store {
     activityStore: ActivityStore;
     commonStore: CommonStore;
+    userStore: UserStore;
 }
 
 export const store: Store = {
     activityStore: new ActivityStore(),
-    commonStore: new CommonStore()
+    commonStore: new CommonStore(),
+    userStore: new UserStore()
 };
 
 export const StoreContext = createContext(store);
