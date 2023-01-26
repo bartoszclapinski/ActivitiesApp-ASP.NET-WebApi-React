9a7380ea9fd582a8595e289305b3b19fa6532af5
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-01-26 21:15:14 +0100

    Activity and user relationship added

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -11,4 +11,23 @@ public class DataContext : IdentityDbContext<AppUser>
     }
 
     public DbSet<Activity> Activities => Set<Activity>();
+    public DbSet<ActivityAttendee> ActivityAttendees { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        
+        builder.Entity<ActivityAttendee>
+            (x => x.HasKey(aa => new { aa.AppUserId, aa.ActivityId }));
+
+        builder.Entity<ActivityAttendee>()
+            .HasOne(u => u.AppUser)
+            .WithMany(a => a.Activities)
+            .HasForeignKey(aa => aa.AppUserId);
+
+        builder.Entity<ActivityAttendee>()
+            .HasOne(u => u.Activity)
+            .WithMany(a => a.Attendees)
+            .HasForeignKey(aa => aa.ActivityId);
+    }
 }
