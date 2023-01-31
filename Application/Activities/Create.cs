b2901f8e1b20b3c72aa0814bdee8db12e9c0e546
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-01-31 21:15:17 +0100

    Create activity handler updated.

--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -1,7 +1,9 @@
 ﻿using Application.Core;
+using Application.Interfaces;
 using Domain;
 using FluentValidation;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace Application.Activities;
@@ -25,15 +27,29 @@ public class Create
     public class Handler : IRequestHandler<Command, Result<Unit>>
     {
         private readonly DataContext _context;
+        private readonly IUserAccessor _userAccessor;
 
-        public Handler(DataContext context)
+        public Handler(DataContext context, IUserAccessor userAccessor)
         {
             _context = context ?? throw new ArgumentNullException(nameof(context));
+            _userAccessor = userAccessor ?? throw new ArgumentNullException(nameof(userAccessor));
         }
 
         public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
         {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.UserName == _userAccessor.GetUsername());
+
+            var attendee = new ActivityAttendee
+            {
+                AppUser = user,
+                Activity = request.Activity,
+                IsHost = true
+            };
+            
+            request.Activity.Attendees.Add(attendee);
+            
             _context.Activities.Add(request.Activity);
+            
             var result = await _context.SaveChangesAsync() > 0;
 
             if (!result) return Result<Unit>.Failure("Failed to create activity");
